9b48c4810761391b27440760292ecd08d0b8cca4
Mateus Sandes <mateussandes@hotmail.com.br>
2024-07-17 12:10:49 -0300

    Adicionado enpoint para create() e atualizado o endpoint findAll() para retornar Users

--- src/main/java/br/com/sandes/controllers/UserController.java
+++ src/main/java/br/com/sandes/controllers/UserController.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.sandes.model.Users;
 import br.com.sandes.services.UserServices;
 
 @RestController
@@ -17,7 +20,12 @@ public class UserController {
 	private UserServices services;
 
 	@GetMapping
-	public List<String> findAll(){
+	public List<Users> findAll(){
 		return services.findAll();
+	}
+	
+	@PostMapping
+	public Users create(@RequestBody Users user) {
+		return services.create(user);
 	}
 }
