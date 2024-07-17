8b365ede9ee348ad2e8df169bf538b96f18b54cf
Mateus Sandes <mateussandes@hotmail.com.br>
2024-07-17 12:11:42 -0300

    Atualizada o metodo findAll() para retornar Users e adicionado o metodo create()

--- src/main/java/br/com/sandes/services/UserServices.java
+++ src/main/java/br/com/sandes/services/UserServices.java
@@ -1,13 +1,34 @@
 package br.com.sandes.services;
 
 import java.util.List;
+import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.sandes.model.Users;
+import br.com.sandes.repositories.UsersRepository;
 
 @Service
 public class UserServices {
-
-	public List<String> findAll(){
-		return List.of("Hello", "World");
+	
+	@Autowired
+	private UsersRepository repository;
+	
+	private static final Logger logger = Logger.getLogger(UserServices.class.getName());
+	
+	public List<Users> findAll(){
+		logger.info("Found all Users on database!");
+		return repository.findAll();
+	}
+	
+	public Users create(Users user) {
+		if(user == null) {
+			throw new IllegalArgumentException("Argument can't be null");
+		}
+		
+		logger.info("User created!");
+		
+		return repository.save(user);
 	}
 }
